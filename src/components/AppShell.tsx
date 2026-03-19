a1a84d9acd6cbf57e5e00ff32972a2bcc3db807a
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2026-03-19 05:01:22 +0000

    chore: iteration 1359 - src/components/AppShell.tsx

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -3,5 +3,5 @@
  * Re-exports from the modularized app-shell directory.
  * @module components/AppShell
  */
-export { AppShell, type ShellRoute, type AppShellProps } from "./app-shell/index";
+export { AppShell, type ShellRoute } from "./app-shell/index";
 export { default } from "./app-shell/index";
